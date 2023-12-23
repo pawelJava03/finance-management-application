a3a5d8bbdcb517fac37bd6918caa4582d7aa9bdb
Paweł <pa.st.a@wp.pl>
2023-12-23 17:10:12 +0100

    Added functionality for creating an account

--- src/main/java/pl/apap/budget_management/Controller.java
+++ src/main/java/pl/apap/budget_management/Controller.java
@@ -12,10 +12,10 @@ import javafx.scene.layout.Pane;
 public class Controller {
 
     @FXML
-    public TextField emailField;
+    public TextField emailField, registerName, registerSurname, registerEmail;
 
     @FXML
-    public PasswordField passwordField;
+    public PasswordField passwordField, registerPassword, registerPassword2;
 
     @FXML
     public Button loginButton;
@@ -51,10 +51,39 @@ public class Controller {
 
     public void register(ActionEvent event) {
         Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Register");
-        alert.setHeaderText(null);
-        alert.setContentText("Przycisk rejestracji został naciśnięty!");
 
-        alert.showAndWait();
+        String name = registerName.getText();
+        String surname = registerSurname.getText();
+        String email = registerEmail.getText();
+        String password = registerPassword.getText();
+        String passwrod2 = registerPassword2.getText();
+
+        if(dbs.userExists(email)){
+            alert.setTitle("Register");
+            alert.setHeaderText(null);
+            alert.setContentText("Account with that email already exists");
+            alert.showAndWait();
+        } else if (name.isBlank()|| name.isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setContentText("Please enter your name");
+            alert.showAndWait();
+        } else if (surname.isBlank()|| surname.isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setContentText("Please enter your surname");
+            alert.showAndWait();
+        }else if(email.isBlank()|| email.isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setContentText("Please enter your email");
+            alert.showAndWait();
+        }else if(!password.equals(passwrod2)) {
+            alert.setTitle("ERROR");
+            alert.setContentText("Passwords are not the same.");
+            alert.showAndWait();
+        }else {
+            dbs.newUser(name, surname, email, password);
+            alert.setTitle("Account created");
+            alert.setContentText("Account created successfully. Now you can login");
+            alert.showAndWait();
+        }
     }
 }
